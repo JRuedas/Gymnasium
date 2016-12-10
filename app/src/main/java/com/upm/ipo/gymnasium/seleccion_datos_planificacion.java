1762b9aea15d4757033426ba9ff6f4b0421afc64
Jonathan <jonathanruedasmora@gmail.com>
2016-12-10 17:20:07 +0100

    Initial commit

---
+++ app/src/main/java/com/upm/ipo/gymnasium/seleccion_datos_planificacion.java
@@ -0,0 +1,13 @@
+package com.upm.ipo.gymnasium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class seleccion_datos_planificacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_datos_planificacion);
+    }
+}
---
+++ app/src/main/java/com/upm/ipo/gymnasium/seleccion_planificacion.java
@@ -0,0 +1,13 @@
+package com.upm.ipo.gymnasium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class seleccion_planificacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_planificacion);
+    }
+}
